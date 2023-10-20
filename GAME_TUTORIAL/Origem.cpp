904c12b9f9010e9095acac5ae560eef97df0e5fb
Benefrancis do Nascimento <benefrancis@gmail.com>
2023-10-20 16:20:49 -0300

    implementando

--- GAME_TUTORIAL/Origem.cpp
+++ GAME_TUTORIAL/Origem.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+
 int main() {
 
-	std::cout << "Samebody gota Die!!\n";
+    std::cout << "Samebody gota Die!!\n";
 
-	system("pause");
+
+    int a = 100;
+    int* aPtr;
+
+    aPtr = &a;
+
+    std::cout << aPtr << std::endl;
+
+    std::cout << &a << std::endl;
+
+
+    int b = 50;
+
+    aPtr = &b;
+
+    std::cout << aPtr << std::endl;
+
+    std::cout << &b << std::endl;
+
+
+    int numbers[] = {0,1,2,3,4,5,6,7,8,9};
+
+
+    std::cout << &b << std::endl;
+
+
+
+
+
+    system("pause");
 
 }
